543c4ab39a555be68b63172b387971de292b5fa8
motch26 <84578100+motch26@users.noreply.github.com>
2023-02-04 11:04:53 +0800

    Add files via upload

---
+++ src/components/PlayerList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Consumer } from "./Context";
+import Player from "./Player";
+
+const PlayerList = () => {
+  return (
+    <Consumer>
+      {({ players }) => {
+        return (
+          <React.Fragment>
+            {players.map((player, index) => (
+              <Player key={player.id.toString()} index={index} />
+            ))}
+          </React.Fragment>
+        );
+      }}
+    </Consumer>
+  );
+};
+
+export default PlayerList;
